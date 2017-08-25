0224cbb14711e1ec4551c4ae6f8695b61534c758
Zoe_guo2016 <Zoe_guo2016@MacdeiMac-2.local>
2017-08-25 23:46:58 +0800

    add post button

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,6 @@ class Group < ApplicationRecord
   belongs_to :user
 
 
-
+  has_many :posts
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-resources :groups
+resources :groups do
+  resources :posts
+end
 
 root 'groups#index'
 
